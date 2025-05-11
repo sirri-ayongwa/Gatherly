c7b65d1f49b2adac4b9588bce08350a77e5af85e
Sirri Ayongwa <sirribongnwi7@gmail.com>
2025-05-12 04:06:19 +0600

    updated footer

--- vite.config.ts
+++ vite.config.ts
@@ -9,9 +9,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
+    react()
   ].filter(Boolean),
   resolve: {
     alias: {
